61b682f59a0fd39531c2d9fdb5e385333d90ee9a
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-03 01:16:52 +0530

    update

---
+++ Searching Algorithm/Binary Search/Binary Search in 2d Array/RowColMatrix.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class RowColMatrix{
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {28, 29, 37, 49},
+            {33, 34, 36, 50},
+
+        };
+
+        int target = 37;
+
+        System.out.println(Arrays.toString(searchIn2d(matrix, target)));
+    }
+
+
+    static int[] searchIn2d(int[][] matrix, int target){
+        int row = 0;
+        int col = matrix.length-1;
+
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target){
+                return new int[]{row, col};
+            }
+            if(matrix[row][col]<target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
