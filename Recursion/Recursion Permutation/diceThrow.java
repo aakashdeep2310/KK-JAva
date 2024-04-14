d528ca1a1b628bb99474c8378e0d99ced3bf7019
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-15 01:34:48 +0530

    update

--- Recursion/Recursion Permutation/diceThrow.java
+++ Recursion/Recursion Permutation/diceThrow.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class diceThrow {
     public static void main(String[] args) {
-        dice("", 4);
+        // dice("", 4);
+        ArrayList<String> ans = diceRec("", 4);
+        System.out.println(ans);
     }
     
     static void dice(String p, int target){
@@ -12,4 +16,19 @@ public class diceThrow {
             dice(p+i, target-i);
         }
     }
+
+    static ArrayList<String> diceRec(String p, int target){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        for(int i = 1; i<= 6 && i<=target; i++){
+            ans.addAll(diceRec(p+i, target-i));
+        }
+        return ans;
+    }
+
+
 }
