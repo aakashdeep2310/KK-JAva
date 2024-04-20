2efd50d0db550f1e020c1af39b015ebec86c3c5c
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-20 20:44:24 +0530

    update

--- Data Structure/Linked List/dll_new.java
+++ Data Structure/Linked List/dll_new.java
@@ -67,24 +67,86 @@ public class dll_new {
             }
 
         }
+        size++;
 
-        
-        
+
+    }
+
+    public void insertAtlastPosition(int value){
+        Node node = new Node(value);
+        Node last = head;
+
+        node.next = null;
+
+        if(head == null){
+            node.prev = null;
+            head = node;
+            return;
+        }
+
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+        System.out.println("Node inserted at last successfuly");
+
 
     }
 
     public void display(){
         Node temp = head;
+        Node last = null;
         while(temp!= null){
             System.out.print(temp.value+ " -> ");
+            last = temp; // while traversing when i reached temp at last make temp as last so we can locate where is last node
             temp = temp.next;
         }
 
         System.out.println("END");
 
+        System.out.println("Print the linkedList in reverse");
+        while (last != null) {
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+
+        System.out.println("Front");
+
+    }
+
+    public void insertAfter(int after, int val){// to insert after any node
+
+        Node p = findNode(after);
+
+        if(p == null){
+            System.out.println("p does not exist");
+            return;
+        }
+
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+
+        if(node.next!=null){
+            node.next.prev = node;
+        }
+
     }
 
 
+    public Node findNode(int val){ // to find the value at which node
+        Node node = head;
+        while (node != null) {
+            if(node.value == val){
+                return node;
+            }
+            node = node.next;
+            
+        }
+        return null;
+    }
 
 
     public static void main(String[] args) {
@@ -94,12 +156,18 @@ public class dll_new {
         list.insertFront(20);
         list.insertFront(10);
 
-        list.display();
+        // list.display();
         
         list.insertLast(50);
         list.insertLast(60);
         list.insertLast(70);
+        // list.display();
+        
+        list.insertAtlastPosition(100);
+        // list.display();
+        list.insertAfter(30, 10000);
         list.display();
+
         
     }
     
