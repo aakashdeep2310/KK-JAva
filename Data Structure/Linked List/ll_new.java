c8450ab32bc4316cd75284a808ca45e9018c30c7
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-20 22:32:10 +0530

    update

--- Data Structure/Linked List/ll_new.java
+++ Data Structure/Linked List/ll_new.java
@@ -82,6 +82,24 @@ public class ll_new {
 
     }
 
+    //insert using recursion
+    public void insertRec(int val, int index){
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node){
+        if(index == 0){
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
+
+    /* ********************* */
+
     public int deleteFirst(){
         int val = head.value;
         head = head.next;
@@ -124,11 +142,11 @@ public class ll_new {
          return value;
     }
 
-<<<<<<< HEAD
-    public Node findNode(int val){ // to find the value at which node
-=======
+
+    // public Node findNode(int val){ // to find the value at which node
+// =======
     public Node findNode(int val){
->>>>>>> fe79f17d8045ff08cd26990234102f2a913e428c
+// >>>>>>> fe79f17d8045ff08cd269902341/02f2a913e428c
         Node node = head;
         while (node != null) {
             if(node.value == val){
@@ -140,11 +158,11 @@ public class ll_new {
         return null;
     }
 
-<<<<<<< HEAD
-    public Node get(int index){ // to find the value of node at any index
-=======
+
+    // public Node get(int index){ // to find the value of node at any index
+
     public Node get(int index){
->>>>>>> fe79f17d8045ff08cd26990234102f2a913e428c
+
         Node node = head;
         for (int i = 0; i < index; i++) {
             node = node.next;
@@ -156,22 +174,55 @@ public class ll_new {
 
     public static void main(String[] args) {
        ll_new list = new ll_new();
-       list.insertFirst(3);
-       list.insertFirst(2);
-       list.insertFirst(8);
-       list.insertFirst(17);
-       list.insertLast(99);
-       list.insertAtindex(100, 3);
-       list.display();
-       System.out.println(list.deleteFirst());
-       list.display();
-       System.out.println(list.deleteLast());
-       list.display();
-       System.out.println(list.deleteIndex(2));
-       list.display();
+    //    list.insertFirst(3);
+    //    list.insertFirst(2);
+    //    list.insertFirst(8);
+    //    list.insertFirst(17);
+    //    list.insertFirst(17);
+    //    list.insertFirst(17);
+    //    list.insertLast(99);
+    //    list.insertAtindex(100, 3);
+    //    list.display();
+    //    System.out.println(list.deleteFirst());
+    //    list.display();
+    //    System.out.println(list.deleteLast());
+    //    list.display();
+    //    System.out.println(list.deleteIndex(2));
+    //    list.display();
 
+    //    list.insertRec(7, 3);
+    //    list.display();
+
+
+    list.insertFirst(4);
+    list.insertFirst(4);
+    list.insertFirst(2);
+    list.insertFirst(1);
+    list.insertFirst(1);
+
+    list.display();
+       list.removeDuplicates();
+        list.display();
       
 
+    }
+
+    // LeetCode Question
+    // Remove Duplicates from linked list (from sorted LinkedList)
+
+    public void removeDuplicates(){
+        Node node = head;
+
+        while(node.next != null){
+            if(node.value == node.next.value){
+                node.next = node.next.next;
+                size--;
+            } else{
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next  = null;
     }
     
 }
