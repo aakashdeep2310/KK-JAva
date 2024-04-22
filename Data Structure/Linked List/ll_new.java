e42c7b26dde7437d40de43e7cc087464d61164a4
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-04-23 02:14:57 +0530

    updated

--- Data Structure/Linked List/ll_new.java
+++ Data Structure/Linked List/ll_new.java
@@ -402,5 +402,35 @@
         }
 
 
+        public ListNode sortList(ListNode head){
+            if(head == null || head.next == null){
+                return head;
+            }
+
+            ListNode mid = getMid(head);
+            ListNode left = sortList(head);
+            ListNode right = sortList(mid);
+
+            return merge(left, right);
+        }
+
+        ListNode merge(ListMode list1, ListNode list2){
+            ListNode dummyHead = new ListNode();
+            ListNode tail = dummyHead;
+            While(list1 != null && list2 != null){
+                if(list1.val < list2.val){
+                    tail.next = list1;
+                    list1 = list1.next;
+                    tail = tail.next;
+                } else {
+                    tail.next = list2;
+                    list2 = list2.next;
+                    tail = tail.next;
+                }
+            }
+            tail.next = (list1 != null) ? list1 : list2;
+            return dummyHead.next;
+        }
+
         
     }
