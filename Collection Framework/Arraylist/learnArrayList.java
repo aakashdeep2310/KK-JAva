468c56dd74c9664788aa04632e552806ea97af74
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-12-10 01:31:01 +0530

    update

---
+++ Collection Framework/Arraylist/learnArrayList.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+
+public class learnArrayList {
+
+    public static void main(String[] args) {
+        // String[] studentName = new String[31];
+        // studentName[0] = "Aakash";
+        // studentName[29] = "Aman";
+        // studentName[30] = "Manit";
+
+
+        // //ArrayList =====>> 
+        // ArrayList<String> students = new ArrayList<>();
+        // students.add("Aakash");
+
+        // List<Integer> list = new ArrayList<>();
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // list.add(4);
+
+        // System.out.println(list);
+        // list.add(5);
+        // System.out.println(list);
+        
+        // list.add(4, 50);
+        // System.out.println(list);
+
+        // List<Integer> newList = new ArrayList<>();
+        // newList.add(150);
+        // newList.add(160);
+        // newList.add(170);
+
+        // list.addAll(newList); // This will add all the element from newList to older list
+        // System.out.println(list);
+
+        // System.out.println(list.get(1));
+
+        // // //remove element from list => remove()
+        // // // list.remove(3);
+        // // System.out.println(list);
+        // // // list.clear();
+        // // // System.out.println(list);
+        
+        // // list.set(3, 45);
+        // // System.out.println(list);
+        
+        
+        // // System.out.println(list.contains(50));
+
+        // //Iterate in the list
+        // for (int i = 0; i < list.size(); i++) {
+        //     System.out.println("The element is: "+ list.get(i));
+        // }
+        
+        // for(Integer element : list){
+        //     System.out.println("foreach element is: "+ element);
+        // }
+
+        // //Iterator
+        // Iterator<Integer> it = list.iterator();
+        // while (it.hasNext()) {
+        //     System.out.println("iterator : " + it.next());
+        // }
+
+
+
+    }
+}
