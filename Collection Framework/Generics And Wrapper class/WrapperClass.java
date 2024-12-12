371688d9945d0ac5d7f62a12fff3d3af2e1ca193
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-12-12 10:08:49 +0530

    update

---
+++ Collection Framework/Generics And Wrapper class/WrapperClass.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        
+        Integer obj = new Integer(12);
+
+        Integer obj2 = Integer.valueOf(13);
+        Integer obj22 = Integer.valueOf("14");
+
+        Boolean myBoolean = Boolean.valueOf(false);
+
+        Integer obj3 = 12; // autoboxing
+
+        int age = obj; // unBoxing
+
+        ArrayList<Integer> a = new ArrayList<>();
+
+
+
+
+    }
+    
+}
