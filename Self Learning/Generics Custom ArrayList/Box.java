f36b1342ef97b27be021a8da5e5275fd822266ee
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-12-14 17:34:11 +0530

    update

---
+++ Self Learning/Generics Custom ArrayList/Box.java
@@ -0,0 +1,16 @@
+// code of Generics class
+public class Box<T> {
+
+    private T content;
+
+    public void setContent(T content){
+        this.content = content;
+    }
+
+    public T getContent(){
+        return content;
+    }
+}
+
+
+
